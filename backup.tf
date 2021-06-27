30219ebc27682fe9ba44a150c67c3fc80b5d4283
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-27 19:36:52 -0300

    Create backup.tf

---
+++ backup.tf
@@ -0,0 +1,16 @@
+/*resource "aws_backup_vault" "webapp-vault" {
+  name = "webapp-vault"
+}
+resource "aws_backup_plan" "webapp-backup-plan" {
+  name = "webapp-backup-plan"
+ 
+  rule {
+    rule_name         = "webapp-backup-rule"
+    target_vault_name = aws_backup_vault.webapp-vault.name
+    schedule          = "cron(0 0 3 ? * SAT *)"
+    lifecycle {
+      cold_storage_after = 90
+      delete_after = 90
+    }
+  }
+}*/
