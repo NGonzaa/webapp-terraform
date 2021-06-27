f42e16a0b83b80b67d5dee21193d2081779709e7
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-27 19:36:37 -0300

    Create efs.tf

---
+++ efs.tf
@@ -0,0 +1,17 @@
+resource "aws_efs_file_system" "webapp-efs" {
+  creation_token = "webapp-efs"
+  availability_zone_name = "us-east-1b"
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+  tags = {
+    Name = "webapp-efs"
+  }
+}
+
+resource "aws_efs_mount_target" "webapp-mt" {
+  file_system_id = aws_efs_file_system.webapp-efs.id
+  subnet_id      = aws_subnet.subnet-webapp-1c.id
+  security_groups = [aws_security_group.sg-efs.id]
+}
