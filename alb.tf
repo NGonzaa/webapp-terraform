b1c0a75a4453f4a2ccab2de0099df5aea62d969d
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-27 19:33:39 -0300

    Create alb.tf

---
+++ alb.tf
@@ -0,0 +1,11 @@
+resource "aws_lb" "webapp-alb" {
+  name   = "webapp-alb"
+  internal   = false
+  load_balancer_type = "application"
+  subnets = [aws_subnet.subnet-webapp-1a.id, aws_subnet.subnet-webapp-1b.id]
+  security_groups = [aws_security_group.sg-alb.id]
+
+  tags = {
+    Name = "webapp-alb"
+  }
+}
