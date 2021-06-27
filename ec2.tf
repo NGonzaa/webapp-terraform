7618a7f757c976ce10895bddf1559b2c5f49af79
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-27 19:36:20 -0300

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,23 @@
+resource "aws_instance" "webapp-server-1" {
+    ami                 = var.ami
+    instance_type       = var.instance_type
+    key_name            = var.key-name
+    vpc_security_group_ids = [aws_security_group.sg-ec2.id]
+    subnet_id = aws_subnet.subnet-webapp-1a.id
+    tags = {
+        Name        = "webapp-server-1"
+        terraform   = "True"
+    }
+}
+
+resource "aws_instance" "webapp-server-2" {
+    ami                 = var.ami
+    instance_type       = var.instance_type
+    key_name            = var.key-name
+    vpc_security_group_ids = [aws_security_group.sg-ec2.id]
+    subnet_id = aws_subnet.subnet-webapp-1b.id
+    tags = {
+        Name        = "webapp-server-2"
+        terraform   = "True"
+    }
+}
