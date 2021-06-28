95fccd4d8c1cde1458cb1abcff1da5bb86fe0286
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-28 20:40:51 -0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -9,7 +9,7 @@ resource "aws_db_subnet_group" "webapp-subnet-group" {
 
 resource "aws_rds_cluster" "rds-webapp" {
   cluster_identifier  = "rds-webapp"
-  db_subnet_group_name = aws_db_subnet_group_name.webapp-subnet-group.name
+  db_subnet_group_name = aws_db_subnet_group.webapp-subnet-group.name
   vpc_security_group_ids = [aws_security_group.sg-rds.id]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   database_name      = "webapp-db"
@@ -23,7 +23,7 @@ resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 2
   identifier         = "rds-webapp-${count.index}"
   cluster_identifier = aws_rds_cluster.rds-webapp.id
-  db_subnet_group_name = aws_db_subnet_group_name.webapp-subnet-group.name
+  db_subnet_group_name = aws_db_subnet_group.webapp-subnet-group.name
   instance_class     = "db.t2.medium"
   engine             = aws_rds_cluster.rds-webapp.engine
   engine_version     = aws_rds_cluster.rds-webapp.engine_version
