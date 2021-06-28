87d3fe61b7b33508b8289d448f7b287b8abb8d65
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-28 20:50:02 -0300

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webapp-server-1" {
     ami                 = var.ami
     instance_type       = var.instance_type
     key_name            = var.key-name
-    vpc_security_group_ids = [aws_security_group.sg-ec2.id]
+    vpc_security_group_ids = [aws_security_group.ec2-sg.id]
     subnet_id = aws_subnet.subnet-webapp-1a.id
     tags = {
         Name        = "webapp-server-1"
@@ -14,7 +14,7 @@ resource "aws_instance" "webapp-server-2" {
     ami                 = var.ami
     instance_type       = var.instance_type
     key_name            = var.key-name
-    vpc_security_group_ids = [aws_security_group.sg-ec2.id]
+    vpc_security_group_ids = [aws_security_group.ec2=sg.id]
     subnet_id = aws_subnet.subnet-webapp-1b.id
     tags = {
         Name        = "webapp-server-2"
