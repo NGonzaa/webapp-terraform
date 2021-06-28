46634fc078e6933b2f08f9f97637650b157b5dde
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-28 20:50:41 -0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "webapp-subnet-group" {
 resource "aws_rds_cluster" "rds-webapp" {
   cluster_identifier  = "rds-webapp"
   db_subnet_group_name = aws_db_subnet_group.webapp-subnet-group.name
-  vpc_security_group_ids = [aws_security_group.sg-rds.id]
+  vpc_security_group_ids = [aws_security_group.rds-sg.id]
   availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
   database_name      = "webapp-db"
   master_username    = "admin"
