f51f763d7e93e050cfeac9de96019b050fd7878e
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-28 21:25:45 -0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -17,6 +17,7 @@ resource "aws_rds_cluster" "rdswebapp" {
   master_password    = "admin123"
   engine             = "aurora-mysql"
   engine_version     = "5.7.mysql_aurora.2.07.2"
+  skip_final_snapshot = true
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
