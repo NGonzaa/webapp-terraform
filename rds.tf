15e79dfda9b9e9e1c51a6f46c7072e10d5a9fc11
NGonzaa <68570957+NGonzaa@users.noreply.github.com>
2021-06-28 21:13:02 -0300

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -7,12 +7,12 @@ resource "aws_db_subnet_group" "webapp-subnet-group" {
   }
 }
 
-resource "aws_rds_cluster" "rds-webapp" {
-  cluster_identifier  = "rds-webapp"
+resource "aws_rds_cluster" "rdswebapp" {
+  cluster_identifier  = "rdswebapp"
   db_subnet_group_name = aws_db_subnet_group.webapp-subnet-group.name
   vpc_security_group_ids = [aws_security_group.rds-sg.id]
   availability_zones = ["us-east-1c", "us-east-1d"]
-  database_name      = "webapp-db"
+  database_name      = "webappdb"
   master_username    = "admin"
   master_password    = "admin123"
   engine             = "aurora-mysql"
@@ -21,10 +21,10 @@ resource "aws_rds_cluster" "rds-webapp" {
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
   count              = 2
-  identifier         = "rds-webapp-${count.index}"
-  cluster_identifier = aws_rds_cluster.rds-webapp.id
+  identifier         = "rdswebapp${count.index}"
+  cluster_identifier = aws_rds_cluster.rdswebapp.id
   db_subnet_group_name = aws_db_subnet_group.webapp-subnet-group.name
   instance_class     = "db.t2.medium"
-  engine             = aws_rds_cluster.rds-webapp.engine
-  engine_version     = aws_rds_cluster.rds-webapp.engine_version
+  engine             = aws_rds_cluster.rdswebapp.engine
+  engine_version     = aws_rds_cluster.rdswebapp.engine_version
 }
